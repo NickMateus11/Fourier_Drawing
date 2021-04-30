c7d1a1b2a233ce546aa990bea606c0b0cbf532d4
NickMateus11 <mateus.nicholas@gmail.com>
2021-04-30 16:31:33 -0400

    save and load drawing

--- mouse_draw.py
+++ mouse_draw.py
@@ -22,6 +22,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                with open('drawing.xy','w') as file:
+                    file.writelines([f'{p[0]},{p[1]}\n' for p in points])
+                    print("saved!")
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_l:
+                with open('drawing.xy','r') as file:
+                    points = [tuple([int(x) for x in p.split(',')]) for p in file.read().strip().split()]
+                    print("loaded!")
 
         # Fill the background with black
         screen.fill(BLACK)
