84d1d4b3dc619d1fbbd3063688263ed1ed55923a
NickMateus11 <mateus.nicholas@gmail.com>
2021-04-30 16:08:59 -0400

    draw with mouse script

---
+++ mouse_draw.py
@@ -0,0 +1,51 @@
+import pygame
+
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+def main():
+    # Set up the drawing window
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+    points = []
+    draw = False
+
+    running = True
+    while running:
+
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Fill the background with black
+        screen.fill(BLACK)
+
+        # collect points if mouse down
+        if pygame.mouse.get_pressed()[0]:
+            if not draw:
+                points = []
+            draw = True
+            pos_tuple = pygame.mouse.get_pos()
+            if not points or not points[-1] == pos_tuple:
+                points.append(pos_tuple)
+        else: 
+            draw = False
+        
+        # draw points
+        if len(points) > 2:
+            pygame.draw.lines(screen, WHITE, False, points, width=2)
+
+        # update screen
+        pygame.display.flip()
+
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
